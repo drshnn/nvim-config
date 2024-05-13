1f6c180701df57aadd8ed644c755d5d11d28b055
drshnn <darshansu156@gmail.com>
2024-05-14 00:05:00 +0530

    basic config

---
+++ lua/plugins.lua
@@ -0,0 +1,4 @@
+--list of all the plugins
+return{
+
+}
---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,14 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        --neo-tree setup
+        vim.keymap.set('n','<leader>n',':Neotree toggle<CR>',{})
+        vim.keymap.set('n','<leader>h',':Neotree<CR>',{})
+    end
+}
