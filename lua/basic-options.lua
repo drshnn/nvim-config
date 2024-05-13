928459c8d1fcb0d565fd85d13bc65ebba4747b21
drshnn <darshansu156@gmail.com>
2024-05-14 00:13:06 +0530

    added line numbers

--- lua/basic-options.lua
+++ lua/basic-options.lua
@@ -2,7 +2,9 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.cmd("set number")
 vim.g.mapleader = " "
+
 
 --basic keymaps
 vim.keymap.set("n", "<leader>cl", function()
